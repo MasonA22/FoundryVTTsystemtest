e018a77dabde11b875b2c1bee69a5bcf049e4147
MasonA22 <masonda2@gmail.com>
2026-05-27 13:34:30 -0500

    test

--- mysystem.mjs
+++ mysystem.mjs
@@ -1,3 +1,27 @@
+class MySystemActorSheet extends ActorSheet {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["mysystem", "sheet", "actor"],
+      template: "systems/mysystem/templates/actor-sheet.hbs",
+      width: 500,
+      height: 400
+    });
+  }
+
+  getData() {
+    const context = super.getData();
+    context.system = this.actor.system;
+    return context;
+  }
+}
+
 Hooks.once("init", () => {
   console.log("mysystem | Initializing");
+
+  Actors.unregisterSheet("core", ActorSheet);
+
+  Actors.registerSheet("mysystem", MySystemActorSheet, {
+    types: ["character"],
+    makeDefault: true
+  });
 });
