5c8218f2e25aa5c2aebc452ab02c6697b798da24
MasonA22 <masonda2@gmail.com>
2026-05-27 13:38:36 -0500

    character sheet test

--- mysystem.mjs
+++ mysystem.mjs
@@ -1,3 +1,36 @@
+const fields = foundry.data.fields;
+
+class CharacterData extends foundry.abstract.TypeDataModel {
+  static defineSchema() {
+    return {
+      health: new fields.SchemaField({
+        value: new fields.NumberField({ initial: 10, integer: true }),
+        max: new fields.NumberField({ initial: 10, integer: true })
+      }),
+
+      abilities: new fields.SchemaField({
+        might: new fields.SchemaField({
+          value: new fields.NumberField({ initial: 0, integer: true })
+        }),
+        agility: new fields.SchemaField({
+          value: new fields.NumberField({ initial: 0, integer: true })
+        }),
+        intellect: new fields.SchemaField({
+          value: new fields.NumberField({ initial: 0, integer: true })
+        })
+      })
+    };
+  }
+}
+
+class WeaponData extends foundry.abstract.TypeDataModel {
+  static defineSchema() {
+    return {
+      damage: new fields.StringField({ initial: "1d6" })
+    };
+  }
+}
+
 class MySystemActorSheet extends ActorSheet {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
@@ -18,8 +51,10 @@ class MySystemActorSheet extends ActorSheet {
 Hooks.once("init", () => {
   console.log("mysystem | Initializing");
 
-  Actors.unregisterSheet("core", ActorSheet);
+  CONFIG.Actor.dataModels.character = CharacterData;
+  CONFIG.Item.dataModels.weapon = WeaponData;
 
+  Actors.unregisterSheet("core", ActorSheet);
   Actors.registerSheet("mysystem", MySystemActorSheet, {
     types: ["character"],
     makeDefault: true
